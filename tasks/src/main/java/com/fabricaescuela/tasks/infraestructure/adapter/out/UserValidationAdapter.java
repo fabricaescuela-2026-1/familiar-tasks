9694050bfda93f2c112c1bfbeca4cd48e1d52456
brayan <brayanjaraba2006@gmail.com>
2026-04-13 10:53:32 -0500

    chore: remove unnecesary imports

--- tasks/src/main/java/com/fabricaescuela/tasks/infraestructure/adapter/out/UserValidationAdapter.java
+++ tasks/src/main/java/com/fabricaescuela/tasks/infraestructure/adapter/out/UserValidationAdapter.java
@@ -2,11 +2,8 @@ package com.fabricaescuela.tasks.infraestructure.adapter.out;
 
 import com.fabricaescuela.tasks.infraestructure.presentation.dtos.MemberHomeDTO;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
