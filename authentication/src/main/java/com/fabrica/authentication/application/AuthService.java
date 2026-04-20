6f9be23b872a826e4b2a7e589518edeb2589730e
brayan <brayanjaraba2006@gmail.com>
2026-04-20 10:59:58 -0500

    chore: remove unnused imports

--- authentication/src/main/java/com/fabrica/authentication/application/AuthService.java
+++ authentication/src/main/java/com/fabrica/authentication/application/AuthService.java
@@ -2,8 +2,6 @@ package com.fabrica.authentication.application;
 
 import java.time.LocalDateTime;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
