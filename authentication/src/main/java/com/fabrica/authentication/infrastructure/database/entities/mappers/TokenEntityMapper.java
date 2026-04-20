1398eeb98ea386d0158ea2eb803d58fc406eeec2
brayan <brayanjaraba2006@gmail.com>
2026-04-20 15:57:56 -0500

    fix: make that tokens use the database user

--- authentication/src/main/java/com/fabrica/authentication/infrastructure/database/entities/mappers/TokenEntityMapper.java
+++ authentication/src/main/java/com/fabrica/authentication/infrastructure/database/entities/mappers/TokenEntityMapper.java
@@ -2,26 +2,31 @@ package com.fabrica.authentication.infrastructure.database.entities.mappers;
 
 import org.springframework.stereotype.Component;
 
+import com.fabrica.authentication.domain.exceptions.UserNotFoundException;
 import com.fabrica.authentication.domain.model.Token;
 import com.fabrica.authentication.infrastructure.database.entities.TokenEntity;
 import com.fabrica.authentication.infrastructure.database.entities.TokenType;
+import com.fabrica.authentication.infrastructure.database.entities.UserEntity;
+import com.fabrica.authentication.infrastructure.database.jpa.UserJpaRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class TokenEntityMapper {
-
+  private final UserJpaRepository userJpaRepo;
   private final UserEntityMapper userEntityMapper;
 
   public TokenEntity toEntity(Token token) {
+    UserEntity userEntity = userJpaRepo.findByEmail(token.getUser().getEmail())
+        .orElseThrow(() -> new UserNotFoundException("User not found"));
     return TokenEntity.builder()
         .tokenId(token.getTokenId())
         .tokenHash(token.getTokenHash())
         .expirationDate(token.getExpirationDate())
         .expiratedAt(token.getExpiratedAt())
         .tokenType(TokenType.valueOf(token.getTokenType()))
-        .user(userEntityMapper.toEntity(token.getUser()))
+        .user(userEntity)
         .build();
   }
 
