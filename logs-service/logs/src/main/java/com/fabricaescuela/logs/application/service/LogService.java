5c89dca8ee874f1cfdeb4c5e37a16257b541ad45
Jose Bernal <josem.bernal@udea.edu.co>
2026-05-03 20:11:36 -0500

    Se crea servicio de logs, se valida su funcionamiento y conexion con base de datos exitosa, CORRECCION DE COMMIT ANTERIOR

--- logs-service/logs/src/main/java/com/fabricaescuela/logs/application/service/LogService.java
+++ logs-service/logs/src/main/java/com/fabricaescuela/logs/application/service/LogService.java
@@ -3,9 +3,13 @@ package com.fabricaescuela.logs.application.service;
 import com.fabricaescuela.logs.domain.model.Log;
 import com.fabricaescuela.logs.domain.ports.in.CreateLogUseCase;
 import com.fabricaescuela.logs.domain.ports.out.LogRepositoryPort;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +18,19 @@ public class LogService implements CreateLogUseCase {
 
     @Override
     @Transactional
-    public Log execute(Log log) {
+    public Log execute(String idUser, String modifiedElement, String action)
+    {
+        Log log = new Log(
+                UUID.randomUUID().toString(),
+                idUser,
+                LocalDateTime.now(),
+                modifiedElement,
+                action
+        );
         return logRepository.save(log);
+    }
+
+    public List<Log> getAllLogs() {
+        return logRepository.findAll();
     }
 }
--- logs-service/logs/src/main/java/com/fabricaescuela/logs/domain/ports/in/CreateLogUseCase.java
+++ logs-service/logs/src/main/java/com/fabricaescuela/logs/domain/ports/in/CreateLogUseCase.java
@@ -3,5 +3,5 @@ package com.fabricaescuela.logs.domain.ports.in;
 import com.fabricaescuela.logs.domain.model.Log;
 
 public interface CreateLogUseCase {
-    Log execute(Log log);
+    Log execute(String idUser, String modifiedElement, String action);
 }
